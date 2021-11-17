6be1b4248cc7f5f39742af3706348a64a33b2c7a
Luisjimenez2812 <luisjimenez2812@gmail.com>
2021-11-17 16:29:00 -0600

    ordenes tomadas dinamico

--- src/app/ordenes-tomadas/ordenes-tomadas.component.ts
+++ src/app/ordenes-tomadas/ordenes-tomadas.component.ts
@@ -9,6 +9,53 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class OrdenesTomadasComponent implements OnInit {
   
+  ordenesTomadas = [
+    {
+      codigo: '000001',
+      nombreCliente: 'Francisca Ramirez',
+      direccion: "Col. 3 de mayo",
+      horaEmision: "12:00",
+      estado: "En camino",
+      productos: [
+        {
+          nombre: "pizza",
+          cantidad: 2,
+        }
+      ],
+      total:200
+    },
+    {
+      codigo: '000005',
+      nombreCliente: 'Francisca Ramirez',
+      direccion: "Col. 3 de mayo",
+      horaEmision: "12:00",
+      estado: "En origen",
+      productos: [
+        {
+          nombre: "pizza",
+          cantidad: 2,
+        }
+      ],
+      total:200
+    },
+    {
+      codigo: '000006',
+      nombreCliente: 'Francisca Ramirez',
+      direccion: "Col. 3 de mayo",
+      horaEmision: "12:00",
+      estado: "En camino",
+      productos: [
+        {
+          nombre: "pizza",
+          cantidad: 2,
+        }
+      ],
+      total:200
+    }
+  ];
+
+  detalleOrden:any;
+
   constructor(private modalService:NgbModal) { }//esta inyectando un objeto de tipo ng modal puedo acceder desde cualquier metodo de la clase haciendo esto
 
   ngOnInit(): void {
@@ -16,12 +63,14 @@ export class OrdenesTomadasComponent implements OnInit {
   faUserCog = faUserCog
   faChevronRight = faChevronRight
   faShippingFast = faShippingFast
-  mostrarModalOrden(modal){
+  mostrarModalOrden(modal, item){
     this.modalService.open(modal,
       {
         size:'md',
         centered:true,
-      })
+      }
+    );
+    this.detalleOrden = item;
   }
 
 }
